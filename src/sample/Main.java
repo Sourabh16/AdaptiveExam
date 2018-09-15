95b5888342578086843639633a49dfd101000ef8
SOURABH-PC\Sourabh <sourabhkolekar16@gmail.com>
2018-09-16 02:05:31 +1000

    all commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello adaptive exam developed by sourabh and saketh");
+        primaryStage.setTitle("Hello adaptive exam");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
